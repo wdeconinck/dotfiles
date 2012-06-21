155922e41fe8abeb874a885764e129d14caa8cb9
Willem Deconinck <wdeconinck@me.com>
2012-06-21 02:46:24 +0200

    symlinks

--- install.sh
+++ install.sh
@@ -4,14 +4,7 @@
 DOTFILES="$( cd "$( dirname "$0" )" && pwd)"
 echo "Installing dotfiles from $DOTFILES into $HOME"
 
-cd $HOME
-
-ln -sf $DOTFILES/bash_profile         .bash_profile
-ln -sf $DOTFILES/git-completion.bash  .git-completion.bash
-ln -sf $DOTFILES/gitignore            .gitignore
-ln -sf $DOTFILES/gitconfig            .gitconfig
-ln -sf $DOTFILES/gitk                 .gitk
-ln -sf $DOTFILES/inputrc              .inputrc
-ln -sf $DOTFILES/pystartup.py         .pystartup.py
+$DOTFILES/decrypt.sh
+$DOTFILES/symlink.sh
 
 echo "Done"
